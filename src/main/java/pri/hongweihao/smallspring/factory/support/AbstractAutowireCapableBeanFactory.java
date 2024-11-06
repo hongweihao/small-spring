21292d178006ccec3ef0abe6df1c86ccc767e80f
hongweihao <karlhong.weihao@qq.com>
2024-11-06 08:48:59 +0800

    merge

--- src/main/java/pri/hongweihao/smallspring/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/pri/hongweihao/smallspring/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,13 +1,13 @@
 package pri.hongweihao.smallspring.factory.support;
 
 import cn.hutool.core.bean.BeanUtil;
-import pri.hongweihao.smallspring.factory.config.BeanDefinition;
 import pri.hongweihao.smallspring.BeanException;
+import pri.hongweihao.smallspring.factory.config.BeanDefinition;
 import pri.hongweihao.smallspring.factory.config.BeanReference;
 import pri.hongweihao.smallspring.factory.config.PropertyValue;
 import pri.hongweihao.smallspring.factory.config.PropertyValues;
 
-import javax.xml.bind.ValidationEvent;
+
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.Optional;
@@ -21,15 +21,19 @@ import java.util.Optional;
  * @date 2022/10/26 13:52
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
-    //private final InstantiationStrategy instantiationStrategy = new JDKInstantiationStrategyImpl();
-    private final InstantiationStrategy instantiationStrategy = new CglibInstantiationStrategyImpl();
+    // private final InstantiationStrategy strategy = new JDKInstantiationStrategyImpl();
+    private final InstantiationStrategy strategy = new CglibInstantiationStrategyImpl();
 
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object... args) {
         Object instance;
-        instance = createInstance(beanName, beanDefinition, args);
+        try {
+            instance = createInstance(beanDefinition, args);
+        } catch (Exception e) {
+            throw new BeanException("Failed to initialize:" + beanDefinition.getBeanClass().getName(), e);
+        }
         applyPropertyValue(beanName, instance, beanDefinition);
-        super.register(beanName, instance);
+        addSingleton(beanName, instance);
         return instance;
     }
 
@@ -38,28 +42,36 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
      * 根据参数列表，获取匹配的构造方法初始化类实例
      * </p>
      *
-     * @param beanName       beanName
      * @param beanDefinition beanDefinition
      * @param args           args
      * @return java.lang.Object
      * @author Karl
      * @date 2022/11/12 14:31
      */
-    private Object createInstance(String beanName, BeanDefinition beanDefinition, Object... args) {
+    @SuppressWarnings("rawtypes")
+    private Object createInstance(BeanDefinition beanDefinition, Object... args) {
         Constructor[] constructors = beanDefinition.getBeanClass().getDeclaredConstructors();
-        Optional<Constructor> first = Arrays.stream(constructors)
-                // 参数个数匹配
-                .filter(constructor -> constructor.getParameterTypes().length == args.length)
-                .findFirst();
+        Optional<Constructor> matchingConstructor = Arrays.stream(constructors)
+                .filter(constructor -> {
+                    // 通过构造方法参数列表长度和类型选择对应的构造方法
+                    Class<?>[] parameterTypes = constructor.getParameterTypes();
+                    if (parameterTypes.length != args.length) {
+                        return false;
+                    }
+                    for (int i = 0; i < parameterTypes.length; i++) {
+                        if (args[i] != null && !parameterTypes[i].isAssignableFrom(args[i].getClass())) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }).findFirst();
+
         try {
-            return instantiationStrategy.createBean(beanDefinition, first.orElse(null), args);
+            return strategy.createBean(beanDefinition, matchingConstructor.orElse(null), args);
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new BeanException("Failed to initialize for " + beanName);
+            throw new BeanException("Failed to initialize " + beanDefinition.getBeanClass().getName(), e);
         }
-
     }
-
 
     private void applyPropertyValue(String beanName, Object instance, BeanDefinition beanDefinition) {
         PropertyValues propertyValues = beanDefinition.getPropertyValues();
